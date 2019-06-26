c39adc43823097539bb2d5b1db952a612f4ecacf
matt bimpson <m.bimpson@gmail.com>
2019-06-26 17:09:44 -0500

    delete old binary, add insert method

--- app.go
+++ app.go
@@ -8,15 +8,23 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var dao = MoviesDAO{}
+
 // AllMovies returns a list of all movies
 func AllMovies(w http.ResponseWriter, r *http.Request) {
 	message := GetMovies()
 	fmt.Fprintln(w, message)
 }
 
+// InsertMovie inserts a single movie
+func InsertMovie(w http.ResponseWriter, r *http.Request) {
+	dao.Insert()
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/movies", AllMovies).Methods("GET")
+	r.HandleFunc("/movies", InsertMovie).Methods("POST")
 	fmt.Printf("api running at port 3000")
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Fatal(err)
